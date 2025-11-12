9e5812d3e286f53dd0c44436bf2c5260b81d4abc
wanz-codename-similarity <xcvispport@gmail.com>
2025-11-13 05:05:36 +0700

    Update users via XCVI Public Bot

--- api/connect.js
+++ api/connect.js
@@ -1241,6 +1241,14 @@ let users = [
     "role": "premium",
     "createdAt": 1762985086440,
     "expired": 1765577086440
+  },
+  {
+    "telegram_id": "8249185700",
+    "username": "zakz",
+    "password": "51b94741",
+    "role": "premium",
+    "createdAt": 1762985136152,
+    "expired": 1765577136152
   }
 ];
 
